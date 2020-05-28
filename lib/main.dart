decfd5a581f4820758b524bf8568b4c877434471
jonathan <pyjonathan31@gmail.com>
2020-05-28 10:39:51 +0200

    tt

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         MyHomePage.routeName: (BuildContext context) => MyHomePage(),
         DetailsPage.routeName: (BuildContext context) => DetailsPage(),
       },
-      home: ResponsivePage(),
+      home: CatsPages(),
     );
   }
 }
